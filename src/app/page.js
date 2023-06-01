ffd383edd87472988aae354812f1ffb9a2753bfc
Eduardo Enriquez <eduardo@guruhotel.com>
2023-05-31 23:33:57 -0600

    fix: comments on text, map and whatsapp link

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex-center">
         <h1 className={`${castoro.className} text-7xl text-center`}>
-          Museo de la Tortura
+          Centro Cultural NEM <br></br>Museo de la Tortura
         </h1>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
@@ -21,53 +21,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>Horarios </h2>
-          <div className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            <li>
-              <b>martes</b> 10:00–19:00
-            </li>
-            <li>
-              <b>miércoles</b> 10:00–19:00
-            </li>
-            <li>
-              <b>jueves</b> 10:00–19:00
-            </li>
-            <li>
-              <b>viernes</b> 10:00–19:00
-            </li>
-            <li>
-              <b>sábado</b> 10:00–19:00
-            </li>
-            <li>
-              <b>domingo</b> 10:00–19:00
-            </li>
-            <li>
-              <b>lunes</b> 10:00–19:00
-            </li>
-          </div>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>Dirección </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            <a href="https://goo.gl/maps/2eXMbwFuuqsn1XfC7?coh=178571&entry=tt">
-              {" "}
-              Maestranza 78 Col.Centro, Guadalajara Jalisco c.p.44100{" "}
-            </a>
-          </p>
-        </a>
-
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -98,18 +52,44 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <h2 className={`mb-3 text-2xl font-semibold`}>Horarios </h2>
+          <div className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            <b>Lunes - Domingo</b> 10:00–19:00 hrs
+          </div>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h2 className={`mb-3 text-2xl font-semibold`}>Contacto </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             <b>Telefono disponible </b> <br></br>
-            <a href="wa.link/taykr6">33 3801 4924</a>
+            <a target="_blank" href="https://wa.link/taykr6">
+              33 3801 4924
+            </a>
           </p>
         </a>
       </div>
-      <div>
-        <p className={castoro.className}>
-          Este sitio continua en construccion...
-        </p>
-      </div>
+
+      <h2 className={`mb-3 text-2xl font-semibold`}>Direccion</h2>
+
+      <iframe
+        src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJv9z1u4mxKIQR7W3f7CY1Z4s&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8`}
+        height="250vh"
+        width="75%"
+        frameBorder="0"
+        style={{ border: 0, filter: "invert(90%)" }}
+        allowFullScreen={true}
+        aria-hidden="false"
+        tabIndex={0}
+        title="Hotel location in Google Maps"
+      />
+
+      <p className={`inline-block align-bottom `}>
+        Este sitio continua en construccion...
+      </p>
     </main>
   );
 }
