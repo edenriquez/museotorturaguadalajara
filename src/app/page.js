05317f13d102868f44732736c70523460fcb59df
Eduardo Enriquez <eduardo@guruhotel.com>
2023-06-05 20:24:47 -0600

    feat: change image by museum one

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,19 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative flex-center">
-        <h1 className={`${castoro.className} text-7xl text-center`}>
+        <h1
+          className={`${castoro.className} text-5xl lg:text-7xl  text-center`}
+        >
           Centro Cultural NEM <br></br>Museo de la Tortura
         </h1>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/skull.png"
+          className="relative"
+          src="/background.png"
           alt="Museo de tortura calabera"
-          width={280}
-          height={137}
+          width={900}
+          height={700}
           priority
         />
       </div>
